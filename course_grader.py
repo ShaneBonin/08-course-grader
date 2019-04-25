a9ae482bbd6d29527cf53613264fe90b3ca028bf
ShaneBonin <49175483+ShaneBonin@users.noreply.github.com>
2019-04-24 19:32:49 -0600

    4/24/19

--- course_grader.py
+++ course_grader.py
@@ -1,2 +1,15 @@
 def course_grader(param):
-    return ""
+     total = 0
+    for score in param:
+        if score < 50:
+            return "fail"
+         
+        total = total + score
+    average = total / len(param)
+    
+    if average < 70:
+        return "fail"
+        
+    
+    
+    return "pass"
